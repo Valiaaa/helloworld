02d5e50ca3277052f39ebb6ffd0eb975231929e0
Valiaaa <valialcc@gmail.com>
2022-09-23 17:15:04 -0400

    update content

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){
 
+    $("#x").click(function(){
+        $("#crossed").show();
+        $("#vanity, #sound, #whispered, #goodbye, #particles").hide();
+        $("#x").hide();
+    });
+
+    $("#vanitypic").click(function(){
+        $("#crossed").hide();
+        $("#sound, #whispered, #goodbye, #particles").hide();
+        $("#vanity").show();
+        $("#x").show();
+    });
+
     $("#soundword").click(function(){
         $("#crossed").hide();
+        $("#vanity, #whispered, #goodbye, #particles").hide();
         $("#sound").show();
+        $("#x").show();
     });
 
-    $("#soundword").hover(function(){
-        $("#crossed").toggle();
-        $("#sound").toggle();
+    $("#whisperedcolor").click(function(){
+        $("#crossed").hide();
+        $("#vanity, #sound, #goodbye, #particles").hide();
+        $("#whispered").show();
+        $("#x").show();
     });
 
-    $("#whisperedcolor").hover(function(){
-        $("#crossed").toggle();
-        $("#whispered").toggle();
+    $("#goodbyedate").click(function(){
+        $("#crossed").hide();
+        $("#vanity, #sound, #whispered, #particles").hide();
+        $("#goodbye").show();
+        $("#x").show();
     });
 
-    $("#vanitypic").hover(function(){
-        $("#crossed").toggle();
-        $("#vanity").toggle();
-    });
-
-    $("#goodbyedate").hover(function(){
-        $("#crossed").toggle();
-        $("#goodbye").toggle();
-    });
-
-    $("#particlestitle").hover(function(){
-        $("#crossed").toggle();
-        $("#particles").toggle();
+    $("#particlestitle").click(function(){
+        $("#crossed").hide();
+        $("#vanity, #sound, #whispered, #goodbye").hide();
+        $("#particles").show();
+        $("#x").show();
     });
 
 
